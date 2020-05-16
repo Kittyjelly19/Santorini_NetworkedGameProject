52c1c7cc9f58f4f08d3c458e3b61e6080d3d8c7c
Kittyjelly19 <49998740+Kittyjelly19@users.noreply.github.com>
2020-05-16 21:10:33 +0100

    updated dlls due to file location errors

--- Santorini/Santorini/Client.cpp
+++ Santorini/Santorini/Client.cpp
@@ -29,41 +29,41 @@ bool Client::RunningClient(unsigned short port)
 	} while (serverName == sf::IpAddress::None);
 
 
-	////Connecting to server and notifying success status. 
+	//Connecting to server and notifying success status. 
 
-	//if (clientSocket.connect(serverName, port) != sf::Socket::Done)
-	//{
-	//	return false;
-	//	cout << "Connection to server: " << serverName << "Successful." << endl;
-	//}
+	if (clientSocket.connect(serverName, port) != sf::Socket::Done)
+	{
+		return false;
+		cout << "Connection to server: " << serverName << "Successful." << endl;
+	}
 
-	//else
-	//{
-	//	cout << "Failed to connect to server: " << serverName << endl;
-	//};
+	else
+	{
+		cout << "Failed to connect to server: " << serverName << endl;
+	};
 
 
-//	//Recieving message from server.
-//
-//	char in[128];
-//	size_t serverReceived;
-//
-//	if (clientSocket.receive(in, sizeof(in), serverReceived) != sf::Socket::Done)
-//	{
-//		return false;
-//
-//		cout << "Received Message from server: \"" << in << "\"" << endl;
-//	};
-//
-//
-//	//Replying to server.
-//
-//	const char out[] = "I am Client 1";
-//	if (clientSocket.send(out, sizeof(out)) != sf::Socket::Done)
-//	{
-//		return false;
-//
-//		cout << "Message Successfully Sent to server: " << serverName << endl;
-//	};
+	//Recieving message from server.
+
+	char in[128];
+	size_t serverReceived;
+
+	if (clientSocket.receive(in, sizeof(in), serverReceived) != sf::Socket::Done)
+	{
+		return false;
+
+		cout << "Received Message from server: \"" << in << "\"" << endl;
+	};
+
+
+	//Replying to server.
+
+	const char out[] = "I am Client 1";
+	if (clientSocket.send(out, sizeof(out)) != sf::Socket::Done)
+	{
+		return false;
+
+		cout << "Message Successfully Sent to server: " << serverName << endl;
+	};
 	return true;
 }
